7146f8966c52c574d6e9bd7669f8a3a6090e1be1
Rafael Mello <ti.rcampanari@gmail.com>
2014-10-28 16:34:45 -0200

    first-commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'), 
+	concat = require('gulp-concat'), 
+    minifyCSS = require('gulp-minify-css');
+    less = require('gulp-less');
+
+var current_module = 'blog';
+
+var paths = {
+		styles : [
+			'less/default.less'
+		]
+};
+
+gulp.task('default',['less'], function() {
+
+});
+
+gulp.task('less', function() {
+
+	gulp.src(paths.styles)
+	 	.pipe(less())
+		.pipe(minifyCSS())
+	    .pipe(concat('styles.min.css'))
+	    .pipe(gulp.dest('assets/dist'));
+
+});
+
